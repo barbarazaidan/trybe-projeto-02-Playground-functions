cbe736f6d845f0b5a31d8d8a9d8183a12e8b8d9d
bárbara-valdez-zaidan <babivaldez@gmail.com>
2022-08-24 19:29:01 -0300

    Resolvi o Desafio 8

--- src/challenges.js
+++ src/challenges.js
@@ -41,15 +41,17 @@ function highestCount(conjuntoValores) {
   // seu código aqui
   let quantidadeMaiorNumero = 0;
   let maiorNumero = conjuntoValores [0];
-  for (let index = 0; index < conjuntoValores.length; index += 1)
-    if (maiorNumero < conjuntoValores [index]) {
-      maiorNumero = conjuntoValores [index];
+  for (let index = 0; index < conjuntoValores.length; index += 1) {
+    if (maiorNumero < conjuntoValores[index]) {
+      maiorNumero = conjuntoValores[index];
     }
-  for (let index2 = 0; index2 < conjuntoValores.length; index2 += 1)  
+  }
+  for (let index2 = 0; index2 < conjuntoValores.length; index2 += 1) {  
     if (maiorNumero === conjuntoValores [index2]) {
       quantidadeMaiorNumero += 1;
     }
     return quantidadeMaiorNumero;
+  } 
 }
 
 // Desafio 7
@@ -67,8 +69,21 @@ function catAndMouse(mouse, cat1, cat2) {
   }
 
 // Desafio 8
-function fizzBuzz() {
+function fizzBuzz(conjuntoNumeros) {
   // seu código aqui
+  let numeroString = [];
+  for (let index = 0; index < conjuntoNumeros.length; index += 1) {
+    if (conjuntoNumeros[index] % 3 === 0 && conjuntoNumeros[index] % 5 === 0) {
+      numeroString.push ("fizzBuzz");
+    } else if (conjuntoNumeros[index] % 3 === 0 && conjuntoNumeros[index] % 5 !== 0) {
+      numeroString.push ("fizz");
+    } else if (conjuntoNumeros[index] % 3 !== 0 && conjuntoNumeros[index] % 5 === 0) {
+      numeroString.push ("buzz");
+    } else {
+      numeroString.push ("bug!");
+    }
+}
+  return numeroString;
 }
 
 // Desafio 9
