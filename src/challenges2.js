1e7082b62c6bd811e99f8f967ceac0ef8f8a4c0f
bárbara-valdez-zaidan <babivaldez@gmail.com>
2022-08-28 19:28:03 -0300

    Resolvi o Desafio 11

--- src/challenges2.js
+++ src/challenges2.js
@@ -1,6 +1,48 @@
 // Desafio 11
-function generatePhoneNumber() {
+function generatePhoneNumber(numeros) {
   // seu código aqui
+  function verificandoTamanho (array) {
+    if (array.length !== 11) {
+      return false;
+  } else {
+      return true;
+    }
+  }
+  function verificandoNumeros (array2) {  
+    for (let contador = 0; contador < array2.length; contador += 1) {
+      let numerosIguais = 1; 
+      if (array2[contador] < 0 || array2[contador] > 9) {
+        return false
+      } else if (array2[contador] >= 0 || array2[contador] < 9 ){
+          for (let contador2 = contador + 1; contador2 < array2.length; contador2 += 1) {
+            if (array2[contador] === array2[contador2]) {
+              numerosIguais += 1;
+            }
+          }
+          if (numerosIguais >= 3) {
+            return false;
+          }
+        } else {
+            return true;
+          };
+    }  
+  }  
+let isTamanhoIncompatível;
+let isNumerosIncompativeis;
+let telefone = "(xx) xxxxx-xxxx";
+isTamanhoIncompatível = verificandoTamanho (numeros);
+isNumerosIncompativeis = verificandoNumeros (numeros);
+
+if (isTamanhoIncompatível === false) {
+  return "Array com tamanho incorreto."
+} else if (isNumerosIncompativeis === false) {
+    return "não é possível gerar um número de telefone com esses valores"
+} else {  
+    for (let index = 0; index < numeros.length; index += 1) {
+      telefone = telefone.replace ("x", numeros [index]);
+    }
+    return telefone;
+  }
 }
 
 // Desafio 12
